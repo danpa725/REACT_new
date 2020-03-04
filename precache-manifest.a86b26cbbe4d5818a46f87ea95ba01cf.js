35c9803e9b15ee7a3f904f2468cf56ddc6db36f0
JuneSung <danpa725@gmail.com>
2020-03-04 18:12:35 +0900

    Updates

---
+++ precache-manifest.a86b26cbbe4d5818a46f87ea95ba01cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c376ee758cbf67484a061e891dcd75",
+    "url": "/REACT_new/index.html"
+  },
+  {
+    "revision": "4e884d59d6770902cbdf",
+    "url": "/REACT_new/static/css/main.92c93b06.chunk.css"
+  },
+  {
+    "revision": "4a6441dd872f86b0b61b",
+    "url": "/REACT_new/static/js/2.36f2e98b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/REACT_new/static/js/2.36f2e98b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e884d59d6770902cbdf",
+    "url": "/REACT_new/static/js/main.d302014f.chunk.js"
+  },
+  {
+    "revision": "5fc16a0a0e4a21a447d4",
+    "url": "/REACT_new/static/js/runtime-main.2d4a226e.js"
+  }
+]);
